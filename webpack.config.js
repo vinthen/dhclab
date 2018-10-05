5210e86be35a5bb10bb3e7a9e11fe4187d0094e0
vinthen <nehtniv@gmail.com>
2018-10-05 08:56:46 +0800

    add homepage file

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./assets/js/",
+  output: {    
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].bundle.js"
+  },
+
+  mode: "development", // "production" | "development" | "none"
+  watch: true,
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: "babel-loader",
+        exclude: /node_modules/
+      } 
+	  
+    ]
+  }
+};
